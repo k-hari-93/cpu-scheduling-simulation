23021f632ab69d7551b960ca600ba9b83473a6f2
Hari K <hari01234@gmail.com>
2014-08-25 21:45:35 +0530

    Created Process List

--- simulate.py
+++ simulate.py
@@ -1,10 +1,16 @@
 import sys
+import os
 
-tq = 15
-cst = 5
+out1 = "out1"
+out2 = "out2"
+out3 = "out3"
+out4 = "out4"
 
-class Process(object):
-    def __int__(self, pname,service_time, arrival_time):
+class FileNotFoundError(Exception):
+    pass
+
+class Proc(object):
+    def __init__(self, pname, service_time, arrival_time):
         self.pname = pname
         self.service_time = service_time
         self.arrival_time = arrival_time
@@ -13,10 +19,39 @@ def main():
     try:
         assert len(sys.argv) == 2
         proc_file = sys.argv[1]
-        print proc_file
+        if not os.path.exists(proc_file):
+            raise FileNotFoundError
+        f = open(proc_file,"r")
 
+        p = f.readline()
+        list = p.split(':')
+        number = int(list[1])
+        p = f.readline()
+        list = p.split(':')
+        tq = int(list[1])
+        p = f.readline()
+        list = p.split(':')
+        cst = int(list[1])
+
+        list = []
+        proc_list = []
+
+        for i in range(number):
+            p = f.readline()
+            list = p.split(':')
+            pname = list[0]
+            service_time = list[1]
+            arr_time = list[2]
+            proc = Proc(pname, service_time, arr_time)
+            proc_list.append(proc)
+
+        f.close()
+    except FileNotFoundError:
+        print "FileError: File Not Found"
     except AssertionError:
-        print "\nUsageError:Please specify exactly one input file.\n"
+        print "UsageError: Please specify exactly one input file"
+
+
 
 
 if __name__ == "__main__":
