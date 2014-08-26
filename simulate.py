b76935c1252262881eac26ed2b7eb6f702c4e792
Hari K <hari01234@gmail.com>
2014-08-26 19:43:01 +0530

    Cleaned Up main(). Getting into the simulation part.

--- simulate.py
+++ simulate.py
@@ -1,51 +1,77 @@
+#!/usr/bin/env python
+
 import sys
 import os
+import operator
 
-out1 = "out1"
-out2 = "out2"
-out3 = "out3"
-out4 = "out4"
+elapsed_time = 0
 
 class FileNotFoundError(Exception):
     pass
 
-class Proc(object):
+class Process(object):
     def __init__(self, pname, service_time, arrival_time):
         self.pname = pname
         self.service_time = service_time
         self.arrival_time = arrival_time
 
+def openfile(f):
+    return open(f,"r+")
+
+def get_params(f):
+    p = f.readline()
+    list = p.split(':')
+    number = int(list[1])
+    p = f.readline()
+    list = p.split(':')
+    tq = int(list[1])
+    p = f.readline()
+    list = p.split(':')
+    cst = int(list[1])
+
+    list = []
+    proc_list = []
+
+    for i in range(number):
+        p = f.readline()
+        list = p.split(':')
+        pname = list[1]
+        service_time = int(list[2])
+        arr_time = int(list[3])
+        proc = Process(pname, service_time, arr_time)
+        proc_list.append(proc)
+
+
+    return number,tq,cst,proc_list
+
+def simulate1(proc_list,out):
+    for i
+
+
 def main():
     try:
         assert len(sys.argv) == 2
         proc_file = sys.argv[1]
+
         if not os.path.exists(proc_file):
             raise FileNotFoundError
         f = open(proc_file,"r")
 
-        p = f.readline()
-        list = p.split(':')
-        number = int(list[1])
-        p = f.readline()
-        list = p.split(':')
-        tq = int(list[1])
-        p = f.readline()
-        list = p.split(':')
-        cst = int(list[1])
+        number,tq,cst,proc_list = get_params(f)
 
-        list = []
-        proc_list = []
+        proc_list = sorted(proc_list,key=operator.attrgetter("arrival_time"))
 
+        '''
         for i in range(number):
-            p = f.readline()
-            list = p.split(':')
-            pname = list[0]
-            service_time = list[1]
-            arr_time = list[2]
-            proc = Proc(pname, service_time, arr_time)
-            proc_list.append(proc)
+            print proc_list[i].arrival_time
+        '''
+        simulate1(proc_list,number,tq,cst,"out1")
+        simulate2(proc_list,number,tq,cst,"out2")
+        simulate3(proc_list,number,tq,cst,"out3")
+        simulate4(proc_list,number,tq,cst,"out4")
 
         f.close()
+
     except FileNotFoundError:
         print "FileError: File Not Found"
     except AssertionError:
